ef35434c6b16a4a4d0f60f317258e5f31bceb49e
unknown <zhug1@CNENZHUG1L1C.corp.emc.com>
2014-10-14 13:37:06 +0800

    l

--- src/main/java/com/easys/webtest/domain/dao/VisitRecordDao.java
+++
@@ -1,8 +0,0 @@
-package com.easys.webtest.domain.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.easys.webtest.domain.entity.VisitRecord;
-
-public interface VisitRecordDao extends JpaRepository<VisitRecord, Long> {
-
-}
--- src/main/java/com/emc/iig/webtest/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.emc.iig.webtest.service;
-
-import com.emc.iig.webtest.domain.entity.User;
-
-public interface UserService {
-
-	public User login(String name, String password);
-
-}
